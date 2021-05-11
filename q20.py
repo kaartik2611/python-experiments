95bbf9125683cf751ffda3dc5c12ac93233a78a6
Kaartik Nayak <kaartiknayak2001@gmail.com>
2021-05-12 00:11:17 +0530

    Added Question 20

---
+++ q20.py
@@ -0,0 +1,4 @@
+print("Format for writing complex number is a+bj")
+c1 = complex(input("Enter First Complex Number: "))
+c2 = complex(input("Enter second Complex Number: "))
+print(f"The sum of {c1} and {c2} is {c1+c2}")
