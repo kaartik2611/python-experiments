e3f4f886414c3f2513ab1062f4a91883d3b41d1f
Kaartik Nayak <kaartiknayak2001@gmail.com>
2021-05-11 18:42:31 +0530

    added qb list
    Co-Authored-By: Alvin Pauly <50576661+avnpl@users.noreply.github.com>

---
+++ q1.py
@@ -0,0 +1,7 @@
+student_name = input("Enter your name: ")
+roll_no = input("Enter Your Roll Number: ")
+marks = float(input("Enter Your 12th Percentage: "))
+if marks >= 40 :
+    print("You are eligible for admission.")
+else:
+    print("You are not eligible for admission.")
