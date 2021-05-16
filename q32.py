1b895d1dbe6e0d39d1ef4ed27f07834f2e295abd
Alvin Pauly <alvinpaul1311@gmail.com>
2021-05-16 20:14:36 +0530

    Updated question 32

--- q32.py
+++ q32.py
@@ -1,18 +1,14 @@
 class Person:
-    no_of_instances = 0                      # Counter variable
+    no_of_instances = 0                         # Counter variable
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.add_instance()                  # Counter will be incremented whenever a object is initiated
+        Person.no_of_instances +=1              # Counter will be incremented whenever a object is initiated
 
-    @classmethod
-    def add_instance(cls):                   # Increment the counter
-        cls.no_of_instances +=1
-
-    @classmethod
-    def get_instance(cls):
-        return cls.no_of_instances           # Static method to get value of counter
+    @staticmethod
+    def get_instance():
+        return Person.no_of_instances           # Static method to get value of counter
 
 person1 = Person('Alvin', 19)
 person2 = Person('Viren', 20)
